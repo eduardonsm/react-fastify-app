fdb4b438a019949c9577eb848278cadacc281944
Eduardo <eduardosume082@gmail.com>
2025-05-02 22:21:42 -0300

    fix files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,14 +6,17 @@ const app = fastify({ logger: true });
 
 const start = async () => {
     await app.register(cors, {
-        origin: 'http://localhost:5173',
+        origin: true,
         methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH']
-    });
+      });
     
     await app.register(routes);
 
     try {
-        await app.listen({ port: 3333 });
+        await app.listen({
+            port: Number(process.env.PORT) || 3333,
+            host: '0.0.0.0'
+          });
         console.log("Server is running on http://localhost:3333");
     } catch (err) {
         process.exit(1);
